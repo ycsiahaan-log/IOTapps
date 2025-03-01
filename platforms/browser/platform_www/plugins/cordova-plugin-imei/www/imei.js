bc80d891a31ec82ee05639e77d7e9445de61bf27
Yoel William Christianson Siahaan <yoelwilliam095@gmail.com>
2025-03-01 23:43:22 +0700

    Delete platforms directory

--- platforms/browser/platform_www/plugins/cordova-plugin-imei/www/imei.js
+++
@@ -1,8 +0,0 @@
-cordova.define("cordova-plugin-imei.IMEIPlugin", function(require, exports, module) { var exec = require('cordova/exec');
-
-module.exports = {
-    get: function(success, error, options) {
-        exec(success, error, "IMEI", "get", [options]);
-    }
-};
-});
